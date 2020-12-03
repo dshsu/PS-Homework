b32d32ecf660672f054ca51dd530da39fdd56080
dshsu <73321543+dshsu@users.noreply.github.com>
2020-12-02 23:13:54 -0500

    Week 2 Homework 2

--- W2HW2.js
+++ W2HW2.js
@@ -56,20 +56,69 @@ return multfinal;
 }
 console.log(sumArray([1,2,3,4])+" and "+ multiplyArray([1,2,3,4]));*/
 //Part 5: Write a function that returns the number of arguments passed to the function when called.
-
+/*
 function manyFunction(input) {
 var args=input.trim().split(" ");
 return args.length;
 }
 console.log(manyFunction("1 2 3 4 5 6"));
+*/
 //part 6: Define a function reverseString that computes the reversal of a string.
 // For example, reverseString("jag testar") should return the string "ratset gaj".
-function reverseString(string) {
-  
+
+/*function reverseString(string) {
+  var newstring=Array.from(string);
+  var reverse="";
+  var count=newstring.length-1;
+  console.log(count);
+  for (var i = count; i >=0; i--) {
+    reverse+=newstring[i];
+  }
+  return reverse;
 }
+console.log(reverseString("whee !"));*/
 
 //part 7:Write a function findLongestWord that takes an array of words and
 //returns the length of the longest one.
 
+/*function findLongestWord(input)
+{
+  var wordlength=0;
+  var finallength=0;
+  var wordarray=input.split(" ");
+for (var i=0 in wordarray)
+{
+  wordlength=wordarray[i].length;
+  if (wordlength>finallength)
+  {
+    finallength=wordlength;
+    i++;
+    }
+  else {
+    i++;
+    }
+  }
+  return finallength;
+}
+
+console.log(findLongestWord("where in the world is carmen sandiego"));
+*/
 //Part 8: Write a function filterLongWords that takes an array of words and a
 //number i and returns the array of words that are longer than i characters long.
+function filterLongWords(input,minlength) {
+var wordarray=input.split(" ");
+var wordlength=0;
+var holdingarray=[];
+for (var i=0 in wordarray) {
+  wordlength=wordarray[i].length;
+    if (wordlength>minlength) {
+      holdingarray.push(wordarray[i]);
+      i++;
+      }
+      else {
+        i++;
+      }
+}
+return holdingarray;
+}
+console.log(filterLongWords("where in the world is carmen sandiego",6));
