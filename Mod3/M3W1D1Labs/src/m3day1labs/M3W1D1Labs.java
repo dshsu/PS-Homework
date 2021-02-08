13382fb2ec0f89be74bfa1d0072e5021cc2efecb
dshsu <73321543+dshsu@users.noreply.github.com>
2021-02-07 21:50:56 -0500

    Establishing Java folder

---
+++ Mod3/M3W1D1Labs/src/m3day1labs/M3W1D1Labs.java
@@ -0,0 +1,32 @@
+package m3day1labs;
+
+public class M3W1D1Labs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+//Morning Lab:
+//	Write a Java program that display the larger of two integers
+//	 Write an if/else statement for the following requirements:
+//	If student gets 90 or higher: console log  A
+//	If students get 80 or above: console log B
+//	If students get 70 or above: console log C
+//	If students get 55 or above: console log D
+//	Any grade lower than 55 is F
+//	 Write a java  program that displays "Good Morning" 
+//	 if time is between 5AM-11:59 | "Good Afternoon" if time is between 12 - 16 | otherwise "Hey there"
+
+//Afternoon Lab:
+//	Using a while loop, print each char of a string ‘Hello World’
+//	Java Program to find duplicate characters in a String
+//	 Use a while loop to add up the numbers 1 to 20 into one variable. Print out the sum at the end
+//	 Use a while loop to print out the even number from 1 to 20. (You'll need Modulus for this. And an IF Statement.) 
+//	 Write a for loop for the given output:
+//	 1, 3, 5, 7, 9
+//	 2, 4, 6, 8, 10
+//	 write a for loop that iterates between 0 and 10 and prints all even numbers. HINT: Read on Module/Remainder operator. 
+//	 Write a for loop that iterates from 1 - 20. Prints “prime” for all prime numbers, “even” for all even numbers, and “odd” 
+//	 for all odd numbers. Treat 2 as an even number and 1 & 3 as odd
