fc904ef54a23d4ea6b89ab6891a8b7c6f6f94e40
Diana Hsu <73321543+dshsu@users.noreply.github.com>
2021-03-10 10:45:49 -0500

    Add W1HW3

---
+++ Mod3/week1hw3/Cuboid.java
@@ -0,0 +1,32 @@
+package week1hw3;
+
+public class Cuboid extends Rectangle{
+	private double height;
+	
+	public Cuboid(double width, double length, double height) {
+		super (width, length);
+		if(height>0) {
+			this.setHeight(height);
+		} else {
+			this.setHeight(0);
+		}
+	}
+	
+
+
+	public double getHeight() {
+		return height;
+	}
+
+
+
+	public void setHeight(double height) {
+		if(height>0)
+		this.height = height;
+	}
+
+	public double getVolume() {
+		return this.height * this.getArea();
+	}
+
+}
